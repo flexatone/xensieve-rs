f4a24eca427695e14f5cc1af028d61a77a8ffd72
Christopher Ariza <ariza@flexatone.com>
2024-02-14 09:54:09 -0500

    made util mode private; implemented mexiriac

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-pub mod util {
 
-    pub fn gcd(mut n: u64, mut m: u64) -> u64 {
+mod util {
+
+    fn gcd(mut n: u64, mut m: u64) -> u64 {
         // not sure if assert is best way to handle this
         assert!(n != 0 && m != 0);
         while m != 0 {
@@ -14,69 +15,89 @@ pub mod util {
         n
     }
 
-    pub fn lcm(a: u64, b: u64) -> u64 {
+    fn lcm(a: u64, b: u64) -> u64 {
         a * b / gcd(a, b)
     }
 
-    pub fn meziriac() -> u64 {
-        42
+    fn meziriac(a: u64, b: u64) -> u64 {
+        let mut g: u64 = 1;
+        if b == 1 {
+            g = 1;
+        } else if a == b {
+            g = 0;
+        } else {
+            while g < u64::MAX {
+                if ((g * a) % b) == 1 {
+                    break
+                }
+                g += 1;
+            }
+        }
+        g
     }
+
+    #[cfg(test)] // only compile when running cargo test
+    mod tests {
+        use super::*; // bring code in outer into scope
+        // use crate::util::*;
+
+        #[test]
+        fn test_gcd_a() {
+            assert_eq!(gcd(14, 15), 1);
+        }
+
+        #[test]
+        fn test_gcd_b() {
+            assert_eq!(gcd(12, 8), 4);
+        }
+
+        #[test]
+        fn test_gcd_c() {
+            let a = 2 * 3 * 5 * 11 * 17;
+            let b = 3 * 7 * 11 * 13 * 19;
+            assert_eq!(gcd(a, b), 3 * 11);
+        }
+
+        #[test]
+        #[should_panic]
+        fn test_gcd_d() {
+            gcd(12, 0);
+        }
+
+        #[test]
+        #[should_panic]
+        fn test_gcd_e() {
+            gcd(0, 3);
+        }
+
+        #[test]
+        fn test_lcm_a() {
+            assert_eq!(lcm(12, 8), 24);
+        }
+
+        #[test]
+        fn test_lcm_b() {
+            assert_eq!(lcm(3, 4), 12);
+        }
+
+        #[test]
+        #[should_panic]
+        fn test_lcm_c() {
+            // as gcd panics on 0, this does as well
+            assert_eq!(lcm(3, 0), 0);
+        }
+
+        #[test]
+        fn test_meziriac_a() {
+            assert_eq!(meziriac(1, 1), 1);
+            assert_eq!(meziriac(10, 1), 1);
+            assert_eq!(meziriac(10, 10), 0);
+            assert_eq!(meziriac(12, 12), 0);
+        }
+    }
+
 }
 
 
 // NOTE: not clear how to test private library methods
-
-#[cfg(test)] // only compile when running cargo test
-mod tests {
-    // use super::*; // bring code in outer into scope
-    use crate::util::*;
-
-    #[test]
-    fn test_gcd_a() {
-        assert_eq!(gcd(14, 15), 1);
-    }
-
-    #[test]
-    fn test_gcd_b() {
-        assert_eq!(gcd(12, 8), 4);
-    }
-
-    #[test]
-    fn test_gcd_c() {
-        let a = 2 * 3 * 5 * 11 * 17;
-        let b = 3 * 7 * 11 * 13 * 19;
-        assert_eq!(gcd(a, b), 3 * 11);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_gcd_d() {
-        gcd(12, 0);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_gcd_e() {
-        gcd(0, 3);
-    }
-
-    #[test]
-    fn test_lcm_a() {
-        assert_eq!(lcm(12, 8), 24);
-    }
-
-    #[test]
-    fn test_lcm_b() {
-        assert_eq!(lcm(3, 4), 12);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_lcm_c() {
-        // as gcd panics on 0, this does as well
-        assert_eq!(lcm(3, 0), 0);
-    }
-
-}
-
 
